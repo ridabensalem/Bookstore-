969354873555f29d7efb8f3a076796cb1aee4157
ridabensalem <ridasbnesalmas@gmail.com>
2023-03-02 15:17:30 +0100

    Update the book store to meet the requirements

--- src/Components/books.jsx
+++ src/Components/books.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
+import Form from './form';
 
-const Books = () => (
-  <>
-    <h2>Hello Books</h2>
-    <li>The new Horizon By david Norson</li>
-    <li>The lands of Arcanum By rida Bensalem</li>
-    <li> The pain By Ahmed sefroui</li>
-    <li>The last day by Victor Hugo</li>
-    <br />
-    <form action="post ">
-      Book Name :
-      {' '}
-      <input type="text" />
-      {' '}
-      <br />
-      {' '}
-      <br />
-      Book Author :
-      {' '}
-      <input type="text" />
-      {' '}
-      <br />
-      {' '}
-      <br />
-      <button type="submit"> add a book</button>
-      {' '}
-      <br />
-    </form>
-  </>
+const Book = ({ title, author }) => (
+  <div>
+    <h2>{title}</h2>
+    <p>
+      by
+      {author}
+    </p>
+  </div>
 );
-export default Books;
+
+const BookList = () => {
+  const books = [
+    { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+    { title: '1984', author: 'George Orwell' },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+  ];
+  const booksWithIds = books.map((book, index) => ({
+    ...book,
+    id: index,
+  }));
+  return (
+    <div>
+      {booksWithIds.map((book) => (
+        <Book key={book.id} title={book.title} author={book.author} />
+      ))}
+      <Form />
+    </div>
+
+  );
+};
+
+export default BookList;
