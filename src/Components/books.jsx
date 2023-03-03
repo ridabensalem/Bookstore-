df8c280f0d4058d01a5622485db278bc60468b2a
ridabensalem <ridasbnesalmas@gmail.com>
2023-03-03 10:09:29 +0100

    add the remove button

--- src/Components/books.jsx
+++ src/Components/books.jsx
@@ -1,34 +1,48 @@
 import React from 'react';
 import Form from './form';
 
-const Book = ({ title, author }) => (
+const Book = ({ title, author, onDelete }) => (
   <div>
     <h2>{title}</h2>
     <p>
       by
+      {' '}
       {author}
+      <button type="submit" onClick={onDelete}>Delete</button>
     </p>
   </div>
 );
 
 const BookList = () => {
-  const books = [
-    { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-    { title: '1984', author: 'George Orwell' },
+  const [books, setBooks] = React.useState([
+    { title: 'To survive a Mockingbird', author: 'Harper Lee' },
+    { title: 'The walker', author: 'George Orwell' },
     { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
-  ];
+  ]);
+
+  const handleDelete = (index) => {
+    const newBooks = [...books];
+    newBooks.splice(index, 1);
+    setBooks(newBooks);
+  };
+
   const booksWithIds = books.map((book, index) => ({
     ...book,
     id: index,
   }));
+
   return (
     <div>
       {booksWithIds.map((book) => (
-        <Book key={book.id} title={book.title} author={book.author} />
+        <Book
+          key={book.id}
+          title={book.title}
+          author={book.author}
+          onDelete={() => handleDelete(book.id)}
+        />
       ))}
       <Form />
     </div>
-
   );
 };
 
