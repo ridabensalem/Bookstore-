8b274304fc9b10d46f3bd33f2fd8aa65949e0417
ridabensalem <ridasbnesalmas@gmail.com>
2023-03-07 13:36:08 +0100

    Update the Bookslice

--- src/redux/Books/BooksSclice.js
+++ src/redux/Books/BooksSclice.js
@@ -1,10 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = [
+  {
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
 const booksSlice = createSlice({
   name: 'books',
-  initialState: {
-    list: [],
-  },
+  initialState,
   reducers: {
     addBook: (state, action) => {
       state.list.push(action.payload);
