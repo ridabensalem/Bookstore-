0775f4383a7ebd86f2e13c1a85c868b6835c22ac
ridabensalem <ridasbnesalmas@gmail.com>
2023-03-08 16:17:15 +0100

    Fixed functionality of remove error

--- src/Components/addBook.jsx
+++
@@ -1,5 +0,0 @@
-const addBook = (book) => ({
-  type: 'ADD_BOOK',
-  payload: book,
-});
-export default addBook();
--- src/redux/Books/BooksSclice.js
+++ src/redux/Books/BooksSclice.js
@@ -1,34 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
+import list from './list';
 
 const initialState = [
-  {
-    item_id: 'item1',
-    title: 'The Great Gatsby',
-    author: 'John Smith',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item2',
-    title: 'Anna Karenina',
-    author: 'Leo Tolstoy',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item3',
-    title: 'The Selfish Gene',
-    author: 'Richard Dawkins',
-    category: 'Nonfiction',
-  },
+  list,
 ];
 const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
     addBook: (state, action) => {
-      state.list.push(action.payload);
+     state= list.push(action.payload);
     },
     removeBook: (state, action) => {
-      state.list = state.list.filter((book) => book.id !== action.payload.id);
+      state = list.filter((book) => book.item_id !== action.payload.id);
     },
   },
 });
