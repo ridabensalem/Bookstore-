395dfd145e178d3a3d77b838fd84d16026e9dc3f
ridabensalem <ridasbnesalmas@gmail.com>
2023-03-11 10:48:36 +0100

    Import the styles into the main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './routes/navBar';
 import BookList from './Components/books';
 import Categories from './Components/categories';
 import Header from './Components/header';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<BookList />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
+    <div className="container">
+      <div className="card">
+        <Header />
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
 
-    </>
+      </div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import store from './redux/store';
 import App from './App';
-import './index.css';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
